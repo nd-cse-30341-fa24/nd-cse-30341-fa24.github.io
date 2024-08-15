5a7452b9302563f69f862486c9ca824db5d031d5
Peter Bui <pbui@nd.edu>
2024-08-15 14:57:06 -0400

    Reading 01: s/checking/testing, use POINTS variable

--- static/txt/reading01/test_program.sh
+++ static/txt/reading01/test_program.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+POINTS=2
+
 output() {
     cat <<EOF | base64 -d
 RmFsc2UKRmFsc2UKRmFsc2UKVHJ1ZQpGYWxzZQpUcnVlCkZhbHNlClRydWUKVHJ1ZQpGYWxzZQpUcnVlCkZhbHNlCg==
 EOF
 }
 
-echo "Checking reading01 program ... "
+echo "Testing reading01 program ... "
 
 DIFF=$(diff <(./program 2> /dev/null) <(output) | grep -E "^[><]" | wc -l)
 COUNT=$(output | wc -l)
 SCORE=$(python3 <<EOF
-print("{:0.2f} / 2.00".format(($COUNT - $DIFF) * 2.0 / $COUNT.0))
+print("{:0.2f} / $POINTS.00".format(($COUNT - $DIFF) * $POINTS.0 / $COUNT.0))
 EOF
 )
 
